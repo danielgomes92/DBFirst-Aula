0640af8849274339940b5ec74d906529c24fe255
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 15:17:16 -0300

    Lista de matriculas finalizada

--- CodeFirst-Aula/DAL/EscolaInitializer.cs
+++ CodeFirst-Aula/DAL/EscolaInitializer.cs
@@ -23,6 +23,40 @@ namespace CodeFirst_Aula.DAL
                 new Aluno { Nome = "Laura", Sobrenome = "Noronha", DataMatricula = DateTime.Parse("2003-09-01")},
                 new Aluno { Nome = "Nilson", Sobrenome = "Oliveira", DataMatricula = DateTime.Parse("2005-09-01")}
             };
+            Alunos.ForEach(s => context.Alunos.Add(s));
+            context.SaveChanges();
+
+            var courses = new List<Curso>
+            {
+                new Curso{CursoID = 1050, Titulo = "Química", Creditos = 3,},
+                new Curso{CursoID = 4022, Titulo = "Microeconomia", Creditos = 3,},
+                new Curso{CursoID = 4041, Titulo = "Macroeconomia", Creditos = 3,},
+                new Curso{CursoID = 1045, Titulo = "Cálculo", Creditos = 4,},
+                new Curso{CursoID = 3141, Titulo = "Trigonometria", Creditos = 3,},
+                new Curso{CursoID = 2021, Titulo = "Composição", Creditos = 3,},
+                new Curso{CursoID = 2042, Titulo = "Literatura", Creditos = 4,},
+            };
+            courses.ForEach(s => context.Cursos.Add(s));
+            context.SaveChanges();
+
+            var matriculas = new List<Matricula>
+            {
+                new Matricula{AlunoID = 1, CursoID = 1050, Nota = Nota.A},
+                new Matricula{AlunoID = 1, CursoID = 4022, Nota = Nota.C},
+                new Matricula{AlunoID = 1, CursoID = 4041, Nota = Nota.B},
+                new Matricula{AlunoID = 2, CursoID = 1045, Nota = Nota.B},
+                new Matricula{AlunoID = 2, CursoID = 3141, Nota = Nota.F},
+                new Matricula{AlunoID = 2, CursoID = 2021, Nota = Nota.F},
+                new Matricula{AlunoID = 3, CursoID = 1050},
+                new Matricula{AlunoID = 4, CursoID = 1050},
+                new Matricula{AlunoID = 4, CursoID = 4022, Nota = Nota.F},
+                new Matricula{AlunoID = 5, CursoID = 4041, Nota = Nota.C},
+                new Matricula{AlunoID = 6, CursoID = 1045},
+                new Matricula{AlunoID = 7, CursoID = 3141, Nota = Nota.A},
+            };
+            matriculas.ForEach(s => context.Matriculas.Add(s));
+            context.SaveChanges();
+
         }
     }
 }
