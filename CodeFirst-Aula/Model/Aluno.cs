659e39ce3bb694b885e55da485791e35d6c6a1de
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 14:37:16 -0300

    inclusão da class aluno e 4 propriedades - ID, Nome, Sobrenome e DateTime

---
+++ CodeFirst-Aula/Model/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_Aula.Model
+{
+    public class Aluno
+    {
+        //ID, Nome, Sobrenome, DataMatricula
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataMatricula { get; set; }
+    }
+}
