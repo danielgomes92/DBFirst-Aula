9434d98f3cc0e476dffa29e864a044c0a188fda1
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 14:33:30 -0300

    inclusão das 4 propriedades - MatriculaID, CursoID, AlunoID e Nota

---
+++ CodeFirst-Aula/Model/Matricula.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_Aula.Model
+{
+    public class Matricula
+    {
+        //MatriculaID, CursoID, ALunoID, Nota
+        public int MatriculaID { get; set; }
+        public int CursoID { get; set; }
+        public int AlunoID { get; set; }
+        public Nota? Nota { get; set; }
+    }
+}
