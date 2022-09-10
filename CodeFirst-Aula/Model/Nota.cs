df7374b7b1c93702230327ed92bf9e03232cb9ed
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 14:25:40 -0300

    criação da class enum Nota dentro da pasta Model

---
+++ CodeFirst-Aula/Model/Nota.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_Aula.Model
+{
+    public enum Nota
+    {
+        A, B, C, D, E, F
+    }
+}
