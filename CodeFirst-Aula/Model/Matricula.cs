82b2adeda5606278df9300dd0b00451723348ecb
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 14:40:14 -0300

    inclusão de duas propriedades para navegação

--- CodeFirst-Aula/Model/Matricula.cs
+++ CodeFirst-Aula/Model/Matricula.cs
@@ -10,8 +10,11 @@ namespace CodeFirst_Aula.Model
     {
         //MatriculaID, CursoID, ALunoID, Nota
         public int MatriculaID { get; set; }
-        public int CursoID { get; set; }
-        public int AlunoID { get; set; }
+        public int CursoID { get; set; }           //chave estrangeira
+        public int AlunoID { get; set; }           //chave estrangeira
         public Nota? Nota { get; set; }
+
+        public virtual Curso Curso { get; set; } // vai ajudar a navegar a partir de um objeto matricula.curso(virtual)
+        public virtual Aluno Aluno { get; set; }
     }
 }
