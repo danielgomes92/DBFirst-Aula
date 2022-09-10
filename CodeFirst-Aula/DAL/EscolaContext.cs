6baebb12c532e4d6075137de21d8159fa85f02c3
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 15:06:46 -0300

    EscolaContext atualizado com DbSet

---
+++ CodeFirst-Aula/DAL/EscolaContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirst_Aula.Model;
+
+namespace CodeFirst_Aula.DAL
+{
+    public class EscolaContext : DbContext // aqui será o contexto a partir do qual o EF pode acessar o DB
+    {
+        public EscolaContext() : base("EscolaContext")
+        {
+        }
+            
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
