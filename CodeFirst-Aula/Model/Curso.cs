74a47b71ff50ac9b63d51c3aafc363edfded01bf
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 14:27:16 -0300

    criação da class Curso

---
+++ CodeFirst-Aula/Model/Curso.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_Aula.Model
+{
+    public class Curso
+    {
+        //CursoID, Titulo e Creditos
+    }
+}
