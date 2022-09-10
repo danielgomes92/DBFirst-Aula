c9c96babb66a882466c964325c34e9367821b4c2
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 15:22:13 -0300

    Finalizado e concluído para testar a aplicação.

--- CodeFirst-Aula/Program.cs
+++ CodeFirst-Aula/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodeFirst_Aula.DAL;
 
 namespace CodeFirst_Aula
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Criando e Inicializando banco de dados a partir do código C#...");
+            using (var db = new EscolaContext())
+            {
+                new EscolaInitializer().InitializeDatabase(db);
+            }
+            Console.WriteLine("Concluído!");
+            Console.ReadKey();
         }
     }
 }
