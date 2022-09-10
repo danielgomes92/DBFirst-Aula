d569eee4cf3dada95ad0066f1e68865c9625b1ad
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 15:07:57 -0300

    criação EscolaInitializer

---
+++ CodeFirst-Aula/DAL/EscolaInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_Aula.DAL
+{
+    public class EscolaInitializer
+    {
+
+    }
+}
